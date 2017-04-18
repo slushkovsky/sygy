acad029723ede16c0e9fe0d5fda37ce256b59837
Sergey Lushkovsky <s.lushkovky@gmail.com>
2017-04-18 11:07:46 -0700

    Created _base.html, Added site pages (quite empty drafts).

--- sygy/urls.py
+++ sygy/urls.py
@@ -4,5 +4,11 @@ import website.views as website
 
 
 urlpatterns = [
-    url(r'^/?$', website.home)
+    url(r'^$', website.home),
+    url(r'^eyeGuest/?$', website.eye_guest),
+    url(r'^eyeWish/?$', website.eye_wish),
+    url(r'^eyeService/?$', website.eye_service),
+    url(r'^eyeMerketer/?$', website.eye_merketer),
+    url(r'^about/?$', website.about_us),
+    url(r'^support/?$', website.support)
 ]
--- website/views.py
+++ website/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_GET
 
 
-@require_http_methods(['GET', 'POST'])
+@require_GET
 def home(request): 
-    if request.method == 'POST': 
-        pass # TODO
-
     return render(request, 'home.html')
      
+@require_GET
+def eye_guest(request): 
+	return render(request, 'eye_guest.html')
+
+@require_GET
+def eye_wish(request): 
+	return render(request, 'eye_wish.html')
+
+@require_GET
+def eye_service(request): 
+	return render(request, 'eye_service.html')
+
+@require_GET
+def eye_merketer(request): 
+	return render(request, 'eye_marketer.html')
+
+@require_GET
+def about_us(request): 
+	return render(request, 'about_us.html')
+
+@require_GET
+def support(request): 
+	return render(request, 'support.html')
